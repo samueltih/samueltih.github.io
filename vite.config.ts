99f531ebbe1f459861e5e8437d12a864154f3b76
Tih Samuel <samuel.tih@outlook.com>
2023-09-20 11:14:53 +0100

    Disable vite-plugin-ssr

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import ssr from "vite-plugin-ssr/plugin";
+// import ssr from "vite-plugin-ssr/plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ssr()],
+  plugins: [react() /*, ssr() */],
 });
